1b1135dd8bd26159cf3eef92a77060099ca54410
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-02-06 20:34:02 +0800

    Finish initial functionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const addBtn = document.getElementById("add-book-btn");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
-const library = [];
+const main = document.getElementById("main");
 
 function Book(title, author, pages) {
   this.title = title;
@@ -14,15 +14,43 @@ function Book(title, author, pages) {
 }
 
 let addNewBook = (event) => {
+  event.preventDefault();
+
   if (title.value === "" || author.value === "" || pages.value === "") {
     alert("Please fill in all the fields");
     return;
   }
   const book = new Book(title.value, author.value, pages.value);
-  library.push(book);
+
   clearInputs();
   closeModal();
-  console.log(library);
+  appendNewBook(book);
+};
+
+let appendNewBook = (book) => {
+  const article = document.createElement("article");
+  article.classList.add("book");
+
+  const div = document.createElement("div");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = book.title;
+
+  const author = document.createElement("p");
+  author.classList.add("author");
+  author.textContent = book.author;
+
+  div.appendChild(h2);
+  div.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.classList.add("pages");
+  pages.textContent = book.pages;
+
+  article.appendChild(div);
+  article.appendChild(pages);
+
+  main.appendChild(article);
 };
 
 let clearInputs = () => {
